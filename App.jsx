caf351b8937ff5bf568117368a862caff458aae7
Amsha007 <47278276+Amsha007@users.noreply.github.com>
2021-07-15 12:16:08 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,9 @@
+import React, { useState } from 'react'
+import data from './data'
+import List from './List'
+function App() {
+	// Edit your return statement here to match the instructions
+	return <h2>reminder project setup</h2>
+}
+
+export default App
